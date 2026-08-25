a507608b6fdf1aaac38d252d206016b0298d3430
gabrielcbarbosa01 <gabrielcbarbosa001@gmail.com>
2026-08-25 14:01:57 -0300

    Initial commit

---
+++ GamingBrainstorm/GamingBrainstorm/GamingBrainstormApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamingBrainstormApp.swift
+//  GamingBrainstorm
+//
+//  Created by Gabriel Barbosa on 25/08/26.
+//
+
+import SwiftUI
+
+@main
+struct GamingBrainstormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
